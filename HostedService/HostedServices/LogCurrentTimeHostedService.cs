21bde7622a905f0784bc0b5f225982168404aa7b
Morné Zaayman <mornezaayman@morzgroup.com>
2022-06-17 20:44:23 +0200

    Add project files.

---
+++ HostedService/HostedServices/LogCurrentTimeHostedService.cs
@@ -0,0 +1,32 @@
+﻿namespace HostedService.HostedServices
+{
+    public class LogCurrentTimeHostedService : IHostedService
+    {
+        private readonly ILogger _logger;
+
+        public LogCurrentTimeHostedService(ILogger<LogCurrentTimeHostedService> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation($"{GetType().Name} is starting.");
+
+            await Task.Delay(3000);
+            _logger.LogInformation($"The current time is {DateTime.Now}.");
+
+            _logger.LogInformation($"{GetType().Name} is finished doing its work in starting.");
+        }
+
+        public async Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation($"{GetType().Name} is stopping.");
+
+            await Task.Delay(3000);
+            _logger.LogInformation($"The current time is {DateTime.Now}.");
+
+            _logger.LogInformation($"{GetType().Name} is finished doing its work in stopping.");
+        }
+    }
+}
